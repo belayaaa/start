bb1da2299ce9d223886e0d2a29860fbce12af574
belayaaa <innabelaya8@gmail.com>
2020-05-04 22:13:22 +0300

    Начальная сборка для великих проектов

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+//Подключаем галп
+const gulp = require('gulp');
+//Объединение файлов
+const concat = require('gulp-concat');
+//Добапвление префиксов
+const autoprefixer = require('gulp-autoprefixer');
+//Оптимизация стилей
+const cleanCSS = require('gulp-clean-css');
+//Оптимизация скриптов
+const uglify = require('gulp-uglify');
+//Удаление файлов
+const del = require('del');
+//Синхронизация с браузером
+const browserSync = require('browser-sync').create();
+//Для препроцессоров стилей
+const sourcemaps = require('gulp-sourcemaps');
+//Sass препроцессор
+const sass = require('gulp-sass');
+//Less препроцессор
+const less = require('gulp-less');
+//Stylus препроцессор
+const stylus = require('gulp-stylus');
+//Модуль для сжатия изображений
+const imagemin = require('gulp-imagemin');
+//Модуль переименовывания файлов
+const rename = require('gulp-rename');
+
+//Порядок подключения файлов со стилями
+const styleFiles = [
+   './src/css/blocks/_container.sass',
+   './src/css/blocks/_intro.sass',
+   './src/css/blocks/_selection.sass',
+   './src/css/blocks/_button.sass',
+   './src/css/blocks/_advantages.sass'
+]
+//Порядок подключения js файлов
+const scriptFiles = [
+   './src/js/lib.js',
+   './src/js/main.js'
+]
+
+//Таск для обработки стилей
+gulp.task('styles', () => {
+   //Шаблон для поиска файлов CSS
+   //Всей файлы по шаблону './src/css/**/*.css'
+   return gulp.src('./src/css/**/*.sass')
+      .pipe(sourcemaps.init())
+      //Указать stylus() , sass() или less()
+      .pipe(sass())
+      //Объединение файлов в один
+      .pipe(concat('style.css'))
+      //Добавить префиксы
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions'],
+         cascade: false
+      }))
+      //Минификация CSS
+      // .pipe(cleanCSS({
+      //    level: 2
+      // }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      //Выходная папка для стилей
+      .pipe(gulp.dest('./build/css'))
+      .pipe(browserSync.stream());
+});
+
+//Таск для обработки скриптов
+gulp.task('scripts', () => {
+   //Шаблон для поиска файлов JS
+   //Всей файлы по шаблону './src/js/**/*.js'
+   return gulp.src('./src/js/**/*.js')
+      //Объединение файлов в один
+      .pipe(concat('main.js'))
+      //Минификация JS
+      .pipe(uglify({
+         toplevel: true
+      }))
+      .pipe(rename({
+         suffix: '.min'
+      }))
+      //Выходная папка для скриптов
+      .pipe(gulp.dest('./build/js'))
+      .pipe(browserSync.stream());
+});
+
+//Таск для очистки папки build
+gulp.task('del', () => {
+   return del(['build/*'])
+});
+
+//Таск для сжатия изображений
+gulp.task('imagemin', ()=> {
+   return gulp.src('./src/images/**')
+   .pipe(imagemin({
+      progressive: true
+   }))
+   .pipe(gulp.dest('./build/images/'))
+});
+
+//Таск для отслеживания изменений в файлах
+gulp.task('watch', () => {
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+   //Следить за добавлением новых изображений
+   gulp.watch('./src/images/**', gulp.series('imagemin'))
+   //Следить за файлами со стилями с нужным расширением
+   gulp.watch('./src/css/**/*.sass', gulp.series('styles'))
+   //Следить за JS файлами
+   gulp.watch('./src/js/**/*.js', gulp.series('scripts'))
+   //При изменении HTML запустить синхронизацию
+   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+//Таск по умолчанию, Запускает del, styles, scripts, img-compress и watch
+gulp.task('default', gulp.series('del', gulp.parallel('styles', 'scripts', 'imagemin'), 'watch'));
